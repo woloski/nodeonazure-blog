8f941554efde3472c5b3db32a980dd432a2afb9d
woloski <woloski@sion.com>
2012-01-03 00:56:42 -0300

    # adding logging to startup task

--- bin/install_nodemodules.cmd
+++ bin/install_nodemodules.cmd
@@ -1,12 +1,14 @@
 cd /d "%~dp0"
 
+echo npm LOG > npmlog.txt
+
 if "%EMULATED%"=="true" exit /b 0
 
 powershell -c "set-executionpolicy unrestricted"
 powershell .\download.ps1 "http://npmjs.org/dist/npm-1.1.0-beta-7.zip"
 
-7za x npm-1.1.0-beta-7.zip -y
-npm install ..\
+7za x npm-1.1.0-beta-7.zip -y >> npmlog.txt
+npm install ..\ >> npmlog.txt
 
 echo SUCCESS
 exit /b 0
--- bin/setup_git.cmd
+++ bin/setup_git.cmd
@@ -1,5 +1,11 @@
 cd /d "%~dp0"
 
+echo LOG > log.txt
+echo EMULATED= %EMULATED% >> log.txt
+echo GITPATH= %GITPATH% >> log.txt
+echo GITREPOBLOGPATH= %GITREPOBLOGPATH% >> log.txt
+echo GITREPOURL= %GITREPOURL% >> log.txt
+
 REM SET GITPATH=C:\Resources\directory\git
 REM SET GITREPOBLOGPATH=C:\Resources\directory\blog
 REM SET GITREPOURL=git://github.com/woloski/nodeonazure-blog.git
@@ -8,12 +14,12 @@ if "%EMULATED%"=="true" exit /b 0
 
 powershell -c "set-executionpolicy unrestricted"
 powershell .\download.ps1 "http://msysgit.googlecode.com/files/PortableGit-1.7.8-preview20111206.7z"
-powershell .\appendPath.ps1 "%GITPATH%"
+powershell .\appendPath.ps1 "%GITPATH%\bin"
 
-7za x PortableGit-1.7.8-preview20111206.7z -y -o"%GITPATH%"
-echo y| cacls "%GITPATH%" /grant everyone:f /t
-"%GITPATH%\git" clone --mirror %GITREPOURL% "%GITREPOBLOGPATH%"
-echo y| cacls "%GITREPOBLOGPATH%" /grant everyone:f /t
+7za x PortableGit-1.7.8-preview20111206.7z -y -o"%GITPATH%" >> log.txt
+echo y| cacls "%GITPATH%" /grant everyone:f /t >> log.txt
+"%GITPATH%\bin\git" clone --mirror %GITREPOURL% "%GITREPOBLOGPATH%" >> log.txt
+echo y| cacls "%GITREPOBLOGPATH%" /grant everyone:f /t >> log.txt
 
 echo SUCCESS
 exit /b 0
