d14fe9bc7d17a6e3a288d07d215517dcd63a50e7
woloski <woloski@sion.com>
2012-01-03 03:51:23 -0300

    # changed to run npm on the root folder instead of bin # added GITREPOBLOGPATH as a system env variable so that node pick it up # added error logging on startup task

--- bin/install_nodemodules.cmd
+++ bin/install_nodemodules.cmd
@@ -8,7 +8,8 @@ powershell -c "set-executionpolicy unrestricted"
 powershell .\download.ps1 "http://npmjs.org/dist/npm-1.1.0-beta-7.zip"
 
 7za x npm-1.1.0-beta-7.zip -y >> npmlog.txt
-npm install ..\ >> npmlog.txt
+cd ..
+bin\npm install >> npmlog.txt
 
 echo SUCCESS
 exit /b 0
--- bin/setup_git.cmd
+++ bin/setup_git.cmd
@@ -1,25 +1,36 @@
 cd /d "%~dp0"
 
-echo LOG > log.txt
-echo EMULATED= %EMULATED% >> log.txt
-echo GITPATH= %GITPATH% >> log.txt
-echo GITREPOBLOGPATH= %GITREPOBLOGPATH% >> log.txt
-echo GITREPOURL= %GITREPOURL% >> log.txt
+echo LOG > setup_git_log.txt
 
-REM SET GITPATH=C:\Resources\directory\git
-REM SET GITREPOBLOGPATH=C:\Resources\directory\blog
+REM SET EMULATED="true"
+REM SET GITPATH=C:\Resources\directory\git\
+REM SET GITREPOBLOGPATH=C:\Resources\directory\blog\
 REM SET GITREPOURL=git://github.com/woloski/nodeonazure-blog.git
+
+REM remove trailing slash if any
+IF %GITPATH:~-1%==\ SET GITPATH=%GITPATH:~0,-1%
+IF %GITREPOBLOGPATH:~-1%==\ SET GITREPOBLOGPATH=%GITREPOBLOGPATH:~0,-1%
+
+echo EMULATED= %EMULATED% 1>> setup_git_log.txt
+echo GITPATH= %GITPATH% 1>> setup_git_log.txt
+echo GITREPOBLOGPATH= %GITREPOBLOGPATH% 1>> setup_git_log.txt
+echo GITREPOURL= %GITREPOURL% 1>> setup_git_log.txt
 
 if "%EMULATED%"=="true" exit /b 0
 
-powershell -c "set-executionpolicy unrestricted"
-powershell .\download.ps1 "http://msysgit.googlecode.com/files/PortableGit-1.7.8-preview20111206.7z"
-powershell .\appendPath.ps1 "%GITPATH%\bin"
+powershell -c "set-executionpolicy unrestricted" 1>> setup_git_log.txt 2>> setup_git_log_error.txt
+powershell .\download.ps1 "http://msysgit.googlecode.com/files/PortableGit-1.7.8-preview20111206.7z" 1>> setup_git_log.txt 2>> setup_git_log_error.txt
+powershell .\appendPath.ps1 "%GITPATH%\bin" 1>> setup_git_log.txt 2>> setup_git_log_error.txt
 
-7za x PortableGit-1.7.8-preview20111206.7z -y -o"%GITPATH%" >> log.txt
-echo y| cacls "%GITPATH%" /grant everyone:f /t >> log.txt
-"%GITPATH%\bin\git" clone --mirror %GITREPOURL% "%GITREPOBLOGPATH%" >> log.txt
-echo y| cacls "%GITREPOBLOGPATH%" /grant everyone:f /t >> log.txt
+7za x PortableGit-1.7.8-preview20111206.7z -y -o"%GITPATH%" 1>> setup_git_log.txt 2>> setup_git_log_error.txt
+echo y| cacls "%GITPATH%" /grant everyone:f /t 1>> setup_git_log.txt 2>> setup_git_log_error.txt
+"%GITPATH%\bin\git" clone --mirror %GITREPOURL% "%GITREPOBLOGPATH%" 1>> setup_git_log.txt 2>> setup_git_log_error.txt
+echo y| cacls "%GITREPOBLOGPATH%" /grant everyone:f /t 1>> setup_git_log.txt 2>> setup_git_log_error.txt
+
+REM add GITREPOBLOGPATH as a system env variable to be used by node
+powershell "& .\addEnvVariable.ps1 GITREPOBLOGPATH %GITREPOBLOGPATH% 'HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Environment'" 1>> setup_git_log.txt 2>> setup_git_log_error.txt
+
+echo y| cacls "%GITREPOBLOGPATH%" /grant everyone:f /t 1>> setup_git_log.txt 2>> setup_git_log_error.txt
 
 echo SUCCESS
 exit /b 0
